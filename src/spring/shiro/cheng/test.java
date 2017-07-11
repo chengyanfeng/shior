9ac1179e681990d5e620fdb26f1f4ebf3193eced
chengyanfeng <569498977@qq.com>
2017-07-11 15:10:34 +0800

    Initial commit

---
+++ src/spring/shiro/cheng/test.java
@@ -0,0 +1,18 @@
+package spring.shiro.cheng;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Administrator on 2017/7/11 0011.
+ */
+@Component
+public class test {
+
+
+            /*public static  void main(String[] args){
+
+                System.out.print("1212");
+            }
+*/
+
+}
---
+++ src/spring/shiro/feng/ShiroRealm.java
@@ -0,0 +1,23 @@
+package spring.shiro.feng;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * Created by Administrator on 2017/7/11 0011.
+ */
+public class ShiroRealm extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
